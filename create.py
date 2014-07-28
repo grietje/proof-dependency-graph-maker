079e38bbe27a3d5ce7045bc28accfce5b2be006b
Grietje Commelin <grietje@wi.th.vu.nl>
2014-07-28 13:55:11 +0200

    Adds create.py

---
+++ create.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+name = input("Name for this pdgm project: ")
+
+print("Creating database:" + name + ".sqlite")
+db = sqlite3.connect(name + ".sqlite")
+cursor = db.cursor()
+
+print("Creating table 'links'")
+cursor.execute("CREATE TABLE links (source, target)")
+db.commit()
+
+db.close()
+
